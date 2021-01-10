4470cceae3121f8cf594538603ec9a3a62bf23c3
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2021-01-09 23:10:18 -0800

    initial work

---
+++ algorithm.py
@@ -0,0 +1,86 @@
+width = 0
+height = 0
+heady = 0
+headx = 0
+
+def set(data):
+    global width
+    width = data["board"]["width"]
+    print(width)
+    global height
+    height = data["board"]["height"]
+    print(height)
+
+def save(data):
+    global headx
+    headx = data["you"]["head"]["x"]
+    global heady
+    heady = data["you"]["head"]["y"]
+
+# This function prevents the snake from running right back into itself
+def reverse(data):
+    global headx
+    horDif = data["you"]["body"][1]["x"] - headx
+    global heady
+    verDif = data["you"]["body"][1]["y"] - heady
+    if (horDif > 0):
+        print(["up", "down", "left"])
+        return ["up", "down", "left"]
+    if (horDif < 0):
+        print(["up", "down", "right"])
+        return ["up", "down", "right"]
+    if (verDif > 0):
+        print(["down", "left", "right"])
+        return ["down", "left", "right"]
+    if(verDif < 0):
+        print(["up", "left", "right"])
+        return ["up", "left", "right"]
+    else:
+      print(["up","down","right","left"])
+      return ["up","down","right","left"]
+
+
+def food(data, move):
+    # This function seeks the food
+    global width, height, headx, heady
+    result = []
+    min = width + height + 1
+    index = 0
+    for i in range(0, len(data["board"]["food"])):
+        toFood = abs(data["board"]["food"][i]["x"] - headx) + abs(data["board"]["food"][i]["y"] - heady)
+        if toFood < min:
+            min = toFood
+            index = i
+    difx = data["board"]["food"][index]["x"] - headx
+    dify = data["board"]["food"][index]["y"] - heady
+    if dify < 0 and "down" in move:
+        result.append("down")
+    if dify > 0 and "up" in move:
+        result.append("up")
+    if difx < 0 and "left" in move:
+        result.append("left")
+    if difx > 0 and "right" in move:
+        result.append("right")
+    if len(result) == 0:
+        print(move)
+        return move
+    print(result)
+    return result
+
+#this function avoids the border
+def border(data,move):
+  global headx, heady
+  if heady + 1 > height:
+    if "up" in move:
+      move.remove("up")
+  if heady - 1 < 0:
+    if "down" in move:
+      move.remove("down")
+  if headx - 1 < 0:
+    if "left" in move:
+      move.remove("left")
+  if headx + 1 > width:
+    if "right" in move:
+      move.remove("right")
+  print(move)
+  return move
--- server.py
+++ server.py
@@ -2,14 +2,23 @@ import os
 import random
 
 import cherrypy
+import json
+from algorithm import *
 
 """
 This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
-
+#these are the global variables
+width = 0
+height = 0
+headx = 0
+heady = 0
 
 class Battlesnake(object):
+
+
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
@@ -19,9 +28,9 @@ class Battlesnake(object):
         return {
             "apiversion": "1",
             "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "color": "#e60000",
+            "head": "shac-tiger-king",
+            "tail": "shac-weight",
         }
 
     @cherrypy.expose
@@ -29,7 +38,7 @@ class Battlesnake(object):
     def start(self):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
-        data = cherrypy.request.json
+        set(cherrypy.request.json)
 
         print("START")
         return "ok"
@@ -38,17 +47,26 @@ class Battlesnake(object):
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
+
         # This function is called on every turn of a game. It's how your snake decides where to move.
         # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
+        # TODO: Use the information in cherrypy.request.json to decide your next move, this gives us a dictionary
         data = cherrypy.request.json
+        save(data)
+        move = ["up", "down", "left", "right"]
+        move = reverse(data)
+        move = food(data,move)
+        move = border(data,move)
+        next = random.choice(move)
+
+
 
         # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+        shouts = ["Impossible Enermy", "That looks delicious", "Hmmm I am hungry", "Grrrrr", "Do I need to know recursion?", "Getting big!"]
+        shout = random.choice(shouts)
 
-        print(f"MOVE: {move}")
-        return {"move": move}
+        print("MOVE:" + next)
+        return {"move": next,"shout" : shout}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -59,6 +77,9 @@ class Battlesnake(object):
 
         print("END")
         return "ok"
+
+
+
 
 
 if __name__ == "__main__":
